32681e266a23b383c1f154d02d75be3e809d8c9e
serta <serta@SRTN-NOTEBOOK>
2024-07-16 18:50:46 +0300

    second change.

--- PatikaTestProject/Program.cs
+++ PatikaTestProject/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("My First Edited Csharp Project");
+Console.WriteLine("My First Edited Csharp Project - second change");
